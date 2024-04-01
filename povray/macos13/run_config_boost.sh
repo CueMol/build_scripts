edb0e241fd66a67669b202390d4e38db3d00835a
ishitani <ishitani@users.sourceforge.net>
2024-04-01 19:29:36 +0900

    povray boost build/install

--- povray/macos13/run_config_boost.sh
+++ povray/macos13/run_config_boost.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+
+set -eux
+
+TMPDIR=$HOME/tmp/tmp
+
+mkdir -p $TMPDIR
+cd $TMPDIR
+
+# get source
+wget https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.bz2
+# cp $HOME/src/boost_1_76_0.tar.bz2 .
+
+tar xzf boost_1_76_0.tar.bz2
+cd boost_1_76_0
+bash bootstrap.sh
+
+#####
+
 instpath=$HOME/proj64/povray_bundle/boost_1_76_static
 
 ./b2 \
@@ -11,3 +29,8 @@ instpath=$HOME/proj64/povray_bundle/boost_1_76_static
  --with-chrono \
  --with-timer \
 link=static threading=multi install
+
+#####
+
+cd ..
+rm -rf boost_1_76_0
