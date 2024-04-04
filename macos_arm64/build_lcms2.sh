f47162c54c0cba402a57494ce244d76fde7d39c2
ishitani <ishitani@users.sourceforge.net>
2024-04-04 21:16:03 +0900

    add lcms2

---
+++ macos_arm64/build_lcms2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eux
+
+BASEDIR=$1
+TMPDIR=$BASEDIR/tmp
+
+mkdir -p $TMPDIR
+cd $TMPDIR
+
+# get source
+wget --progress=dot:mega https://github.com/mm2/Little-CMS/releases/download/lcms2.15/lcms2-2.15.tar.gz
+tar xzf lcms2-2.15.tar.gz
+cd lcms2-2.15
+
+#####
+
+INSTPATH=$BASEDIR/builds/lcms2-2.15
+
+./configure \
+    --prefix=$INSTPATH \
+    --enable-static --disable-shared
+
+make -j 8
+make install
