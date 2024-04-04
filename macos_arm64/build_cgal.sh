602bd37bae1f464b58cd3316c341de0b16384f9a
ishitani <ishitani@users.sourceforge.net>
2024-04-04 21:32:54 +0900

    add build cgal

---
+++ macos_arm64/build_cgal.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eux
+
+BASEDIR=$1
+TMPDIR=$BASEDIR/tmp
+
+mkdir -p $TMPDIR
+cd $TMPDIR
+
+# get source
+wget --progress=dot:mega https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.14.3/CGAL-4.14.3.tar.xz
+tar xJf CGAL-4.14.3.tar.xz
+cd CGAL-4.14.3
+
+#####
+
+INSTPATH=$BASEDIR/builds/CGAL-4.14.3
+
+cmake .. \
+      -DCMAKE_INSTALL_PREFIX=$INSTPATH \
+      -DCMAKE_BUILD_TYPE="Release" \
+      -DCMAKE_C_COMPILER=clang \
+      -DCMAKE_CXX_COMPILER=clang++ \
+      -DCMAKE_CXX_FLAGS="-std=c++14" \
+      -DBOOST_ROOT=$BASEDIR/builds/boost_1_84/ \
+      -DWITH_CGAL_Qt5=OFF \
+      -DWITH_CGAL_ImageIO=OFF \
+      -DCGAL_DISABLE_GMP=TRUE \
+      -DBUILD_SHARED_LIBS=FALSE
+
+make -j 8
+make install
+
--- macos_arm64/build_glew.sh
+++ macos_arm64/build_glew.sh
@@ -8,7 +8,8 @@ mkdir -p $TMPDIR
 cd $TMPDIR
 
 # get source
-wget --content-disposition https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0.tgz/download
+wget --content-disposition \
+     https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0.tgz/download
 tar xzf glew-2.1.0.tgz
 cd glew-2.1.0
 
@@ -16,4 +17,8 @@ cd glew-2.1.0
 
 INSTPATH=$BASEDIR/builds/glew-2.1.0
 
-make GLEW_PREFIX=$INSTPATH GLEW_DEST=$INSTPATH install
+make \
+    CC="clang -Wno-strict-prototypes -Wdeprecated-declarations" \
+    GLEW_PREFIX=$INSTPATH \
+    GLEW_DEST=$INSTPATH \
+    install
