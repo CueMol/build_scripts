b7e4229bb5846da7272a3ce03ccb3dd12be72e91
ishitani <ishitani@users.sourceforge.net>
2024-04-04 21:22:46 +0900

    add glew

---
+++ macos_arm64/build_glew.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eux
+
+BASEDIR=$1
+TMPDIR=$BASEDIR/tmp
+
+mkdir -p $TMPDIR
+cd $TMPDIR
+
+# get source
+wget --content-disposition https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0.tgz/download
+tar xzf glew-2.1.0.tgz
+cd glew-2.1.0
+
+#####
+
+INSTPATH=$BASEDIR/builds/glew-2.1.0
+
+make GLEW_PREFIX=$INSTPATH GLEW_DEST=$INSTPATH install
