769f9b26c6ec98d23285a3224daac8b404a6a227
ishitani <ishitani@users.sourceforge.net>
2024-04-04 19:17:00 +0900

    add actions wf

--- macos_arm64/build_boost.sh
+++ macos_arm64/build_boost.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
-instpath=$HOME/proj64/boost_1_84
+set -eux
+
+BASEDIR=$1
+TMPDIR=$BASEDIR/tmp
+
+mkdir -p $TMPDIR
+cd $TMPDIR
+
+# get source
+wget --progress=dot:giga https://boostorg.jfrog.io/artifactory/main/release/1.84.0/source/boost_1_84_0.tar.bz2
+
+tar xjf boost_1_84_0.tar.bz2
+cd boost_1_84_0
+bash bootstrap.sh
+
+#####
+
+instpath=$BASEDIR/builds/boost_1_84
 
 ./b2 \
  --prefix=$instpath \
@@ -10,6 +27,7 @@ instpath=$HOME/proj64/boost_1_84
  --with-thread \
  --with-chrono \
  --with-timer \
+ -d0 \
 link=shared threading=multi install
 
 # architecture=x86 address-model=64 
