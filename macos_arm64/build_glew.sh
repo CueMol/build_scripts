522cbce131876f441dc5b4f4bdb4a8bbebd13a82
ishitani <ishitani@users.sourceforge.net>
2024-04-20 15:18:35 +0900

    refactor build glew

--- macos_arm64/build_glew.sh
+++ macos_arm64/build_glew.sh
@@ -2,6 +2,8 @@
 set -eux
 
 BASEDIR=$1
+RUNNER_OS=$2
+RUNNER_ARCH=$3
 TMPDIR=$BASEDIR/tmp
 
 mkdir -p $TMPDIR
@@ -15,10 +17,19 @@ cd glew-2.1.0
 
 #####
 
-INSTPATH=$BASEDIR/builds/glew-2.1.0
+INSTPATH=$BASEDIR/glew-2.1.0
+
+if [ $RUNNER_OS = "macOS" ]; then
+    CC="clang -Wno-strict-prototypes -Wdeprecated-declarations"
+elif [ $RUNNER_OS = "Linux" ]; then
+    CC="gcc"
+else
+    echo "unknown runner os: $RUNNER_OS"
+    exit 1
+fi
 
 make \
-    CC="clang -Wno-strict-prototypes -Wdeprecated-declarations" \
+    CC=$CC \
     GLEW_PREFIX=$INSTPATH \
     GLEW_DEST=$INSTPATH \
     install
