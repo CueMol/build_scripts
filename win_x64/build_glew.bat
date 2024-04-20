9d1b5cda297b7c9cd8616c2728a1bafa927f0af4
ishitani <ishitani@users.sourceforge.net>
2024-04-20 15:07:04 +0900

    .

--- win_x64/build_glew.bat
+++ win_x64/build_glew.bat
@@ -1,5 +1,5 @@
 echo on
-wget https://github.com/nigels-com/glew/releases/download/glew-2.2.0/glew-2.2.0.tgz
+wget --progress=dot:mega https://github.com/nigels-com/glew/releases/download/glew-2.2.0/glew-2.2.0.tgz
 tar xzf glew-2.2.0.tgz
 cd glew-2.2.0
 
